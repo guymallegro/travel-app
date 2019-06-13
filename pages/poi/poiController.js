1d5eaa84a92cb99a3b02334ae40416067e99b216
Guy Marushchenko <guym@allegro.ai>
2019-06-13 17:13:35 +0300

    add poi request

--- pages/poi/poiController.js
+++ pages/poi/poiController.js
@@ -1,7 +1,12 @@
 angular.module("myApp")
 .controller("poiController", function ($scope,$http) {
     self = this;
-    $http.get('http://localhost:3000/hello').then(function(response){
-        $scope.myWelcome=response.data;
+    $http.get('http://localhost:3000/poi/getAll')		.then(function (response){
+        $scope.jsondata = response.data;
+        console.log("status:" + response.status);
+    }).catch(function(response) {
+      console.error('Error occurred:', response.status, response.data);
+    }).finally(function() {
+         console.log("Task Finished.");
     });
 });
