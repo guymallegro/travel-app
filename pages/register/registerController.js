853151268c14a4e696337a184d49489a831e39f6
Guy Marushchenko <guym@allegro.ai>
2019-06-15 12:51:29 +0300

    working registration

--- pages/register/registerController.js
+++ pages/register/registerController.js
@@ -1,14 +1,24 @@
 angular.module("myApp")
-.controller("registerController", function ($scope,$http) {
-    $scope.login = function(userName, password){
-        $http.post('http://localhost:3000/users/login',{userName:userName,password:password}).then(function (response){
-            $window.localStorage.setItem('token-vacation-api',data);
-            $scope.$root.token = $window.localStorage.getItem('token-vacation-api');
-            $scope.$root.currentUser=userName;
-        }).catch(function(response) {
+.controller("registerController", function ($scope,$http,$location) {
+    $http.get('http://localhost:3000/POI/getCategories').then(function (response){
+        $scope.interests=response.data;
+    }).catch(function(response) {
+      console.error('Error occurred:', response.status, response.data);
+    }).finally(function() {
+         console.log("Task Finished.");
+    });
+
+    $scope.register = function(userName, password, firstName, lastName, country, city, email, firstQuestion, secondQuestion, firstAnswer, secondAnswer, firstInterest, secondInterest){
+        $http.post('http://localhost:3000/users/register',{userName:userName,password:password, firstName:firstName, lastName:lastName, country:country, city:city, email:email, firstQuestion:firstQuestion, secondQuestion:secondQuestion, firstAnswer:firstAnswer, secondAnswer:secondAnswer, firstInterest:firstInterest, secondInterest:secondInterest}).then(function (response){
+            $location.url("/login")
+    }).catch(function(response) {
           console.error('Error occurred:', response.status, response.data);
         }).finally(function() {
              console.log("Task Finished.");
         });
     };
+
+    $scope.countries=["Australia","Bolivia","China","Denemark","Israel","Latvia","Monaco","August","Norway","Panama","Switzerland","USA"]
+    $scope.firstQuestions=["What is your middle name?", "What is your favorite book?"]
+    $scope.secondQuestions=["What was your firsts pet name?", "What is your favorite book?"]
 });
