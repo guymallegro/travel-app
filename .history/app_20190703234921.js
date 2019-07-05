a79ce8280f5e56c0911144d597bb2344134b9956
sapirratzon <sapir.ratzon23@gmail.com>
2019-07-05 17:40:38 +0300

    start design home

---
+++ .history/app_20190703234921.js
@@ -0,0 +1,55 @@
+let app = angular.module('myApp', ["ngRoute"]);
+
+// config routes
+app.config(function($routeProvider,$windowProvider)  {
+    var $window = $windowProvider.$get();
+    if($window.sessionStorage.getItem('vacation-token')){
+        loggedIn=true;
+    }
+    else{
+        loggedIn=false;
+    }
+    
+    $routeProvider
+        .when('/', {
+            templateUrl: function() {
+                return loggedIn ? 'pages/homeUsers/homeUsers.html' : 'pages/homeGuests/homeGuests.html';
+              },
+            controller: function() {
+                return loggedIn ? 'homeUsersController as userCtrl' : 'homeGuestsController as guestCtrl';
+              },
+        }).when('/homeGuests', {
+            templateUrl: 'pages/homeGuests/homeGuests.html',
+            controller : 'homeGuestsController as guestCtrl'
+        }).when('/homeUsers', {
+            templateUrl: 'pages/homeUsers/homeUsers.html',
+            controller : 'homeUsersController as userCtrl'
+        }).when('/about', {
+            templateUrl: 'pages/about/about.html',
+            controller : 'aboutController as abtCtrl'
+        }).when('/chosenPOI', {
+            templateUrl: 'pages/chosenPOI/chosenPOI.html',
+            controller : 'chosenPOIController as cpoiCtrl'
+        }).when('/forgotPassword', {
+            templateUrl: 'pages/forgotPassword/forgotPassword.html',
+            controller : 'forgotPasswordController as forgotPassCtrl'
+        }).when('/poi', {
+            templateUrl: 'pages/poi/poi.html',
+            controller : 'poiController as poiCtrl'
+        }).when('/login', {
+            templateUrl: 'pages/login/login.html',
+            controller : 'loginController as loginCtrl'
+        }).when('/favorites', {
+            templateUrl: 'pages/favorites/favorites.html',
+            controller : 'favoritesController as favCtrl'
+        }).when('/register', {
+            templateUrl: 'pages/register/register.html',
+<<<<<<< Updated upstream
+            controller : 'registerController as regCtrl'
+=======
+        controller : 'registerController as poiCtrl'
+>>>>>>> Stashed changes
+        })
+        
+        .otherwise({ redirectTo: '/' });
+});
---
+++ .history/pages/homeGuests/homeGuestsController_20190705144300.js
@@ -0,0 +1,67 @@
+angular.module("myApp")
+.controller("homeGuestsController", function ($scope,$http,$location,$window) {
+  $scope.pointName = "";
+  $scope.$root.login = true;
+  $scope.$root.register = true;
+  // $scope.$root.logout = false;
+  $scope.$root.currentUser = "Guest";
+    $http.get('http://localhost:3000/poi/getAll').then(function (response){
+        result = response.data;
+        var points=[]
+        for(i=0;i<result.length;i++) { 
+            if (result[i].rank > 3) {
+                points.push(result[i]);  
+            }
+          }
+          var randomPositions = []
+          $scope.threeRandomPoints=[]
+
+          while(randomPositions.length < 3){
+              var r = Math.floor(Math.random()*(points.length-1)) + 1;
+              if(randomPositions.indexOf(r) === -1){
+                randomPositions.push(r);
+                $scope.threeRandomPoints.push(points[r])
+              } 
+          }
+        
+
+    }).catch(function(response) {
+      console.error('Error occurred:', response.status, response.data);
+    }).finally(function() {
+         console.log("Task Finished.");
+    });
+
+    if($window.sessionStorage.getItem('vacation-token')){
+      $scope.loggedIn=true;
+      $scope.currentUser=$window.sessionStorage.getItem('vacation-user-name')
+
+  }
+  else{
+      $scope.loggedIn=false;
+  }
+
+  
+  
+  $scope.openPOIPage = function (poiName){
+    pointName = poiName.poiName;
+    $location.url("/chosenPOI")
+}
+
+    $scope.moveToRegister = function(){
+        $location.url("/register")
+    }
+
+    $scope.moveToLogin = function(){
+        $location.url("/login");
+    }
+
+    $scope.logout = function (){
+        $scope.$root.currentUser = "Guest";
+        $scope.login = false;
+        $scope.register = false;
+        $scope.logout = false;
+        $location.url("/homeGuests");
+        $window.sessionStorage.clear();
+    }
+    
+});
